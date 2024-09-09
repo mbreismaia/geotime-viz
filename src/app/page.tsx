f9ec2fc6c7e321762588699172d909053deb7a3c
Beatriz Maia <prbeatrizmaia@gmail.com>
2024-09-09 14:28:57 -0300

    creating menu component and installing lucide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
    <div>
     This is a SpatioTemporal  Dashboard
+    <Sidebar />
    </div>
   );
 }
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+   <div>
+    This is a Sidebar
+   </div>
+  );
+}
