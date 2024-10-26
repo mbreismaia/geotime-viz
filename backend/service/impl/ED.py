66e04153b810a266115fe18637b8c9f6ac02eb5b
Edson Neto <emn2@cin.ufpe.br>
2024-10-25 22:31:40 -0300

    add plots backend requisitions

---
+++ backend/service/impl/ED.py
@@ -0,0 +1,85 @@
+from quicksort import *
+import numpy as np
+from depth.multivariate import *
+from backend.schemas.queryED import QueryED
+
+def calculatePointwiseDepth(C, variables, pos, depth_type='L2'):
+    data = []
+    for i in range(len(C)):
+        list = [C[i].data[variables[j]][pos] for j in range(variables)]
+        data.append(np.array(list))
+
+    data = np.array(data)
+
+    depths = []
+    if depth_type == 'L2':
+        depths = L2(data, data)
+    elif depth_type == 'Spatial':
+        depths = spatial(data, data)
+    elif depth_type == 'mahalanobis':
+        depths = mahalanobis(data, data)
+    else:
+        depths = halfspace(data, data)
+
+    for i in range(len(C)):
+        C[i].depth_g[pos] = depths[i]
+
+    return 
+
+def ED(C, query: QueryED):
+
+    N = len(C)                                       # amount of data
+    P = len(C[0].P)                                  # resolution of data
+
+    r = query.r
+    r_size = len(query.r)                            # Auxiliary vector used by extremal depth
+    depth_type = query.depth_type                    # type of depth being used
+    variables = len(query.variables)                 # amount of variables being analyzed
+    leftHour, rightHour = query.hour_interval        # interval of hours being analyzed
+
+    print("N: ", N)
+    print("P: ", P)
+    print("variables: ", variables)
+
+    if variables == 1:
+        variable = query.variables[0]
+        print("is univariate")
+        for g in range(N):
+            for t in range(leftHour, rightHour + 1):
+                for f in range(N):
+                    if C[f].data[variable][t] < C[g].data[variable][t]:
+                        C[g].depth_g[t] += 1.0
+                    if C[f].data[variable][t] > C[g].data[variable][t]:
+                        C[g].depth_g[t] -= 1.0
+                C[g].depth_g[t] = 1 - (abs(C[g].depth_g[t]) / N)
+    else:
+        print("is multivariate")
+        for t in range(leftHour, rightHour + 1):
+            calculatePointwiseDepth(C, variables, t, depth_type)
+ 
+    for g in range(N):
+        for rr in range(r_size):
+            cnt = 0
+            for t in range(leftHour, rightHour + 1):
+                if C[g].depth_g[t] <= r[rr]:
+                    cnt += 1
+            C[g].phi[rr] = cnt / (rightHour - leftHour + 1)
+
+    # order functions
+    quicksort(C, 0, N - 1)
+
+    for i in range(N):
+        L = 0
+        R = N - 1
+        while L <= R:
+            mid = (L + R) // 2
+            if cmp(C[i], C[mid]):
+                R = mid - 1
+            else:
+                L = mid + 1
+
+        C[i].extremal_depth = L / N
+
+    C = C[::-1]
+
+    return 
---
+++ backend/service/impl/plots_service.py
@@ -0,0 +1,171 @@
+import json
+from ED import ED
+import numpy as np
+import pandas as pd
+from datetime import *
+from curve import Curve
+import umap.umap_ as umap
+from sklearn.manifold import TSNE
+from backend.schemas.queryED import QueryED
+from backend.schemas.parameters import Parameters
+
+class PlotService:
+    @staticmethod
+    def getDate(num):
+        year = 2012
+        dt = date(year, 1, 1) + timedelta(num - 1)
+        day = dt.weekday()
+        month = dt.month
+        return (dt, day, month)
+    
+    # Funcao para ser chamada no começo quando a aplicação é rodada pela primeira vez para ler os dados
+    @staticmethod
+    def read_data_initial():
+        curves = pd.read_csv('./backend/db/curvesWithLocationOrigin.csv')
+
+        N = 366
+        P = 24
+        R = 100
+        Z = 266          #amount of zones
+
+        variables = ['values', 'prices', 'distances', 'total_time']
+
+        C = []                                       
+        for i in range(N):
+            C.append(Curve(P, i, R, variables))
+
+        matrixes = {}
+        for var in variables:
+            matrixes[var] = np.empty((Z, N), dtype = object)
+
+        for i in range(len(curves)):
+            id, zone = curves.at[i, 'id'], curves.at[i, 'zone']
+            for var in variables:
+                val = np.array(list(map(float, curves.at[i, var][1:-1].split(', '))))
+                matrixes[var][zone][id] = val
+
+        for i in range(N):
+            for j in range(Z):
+                C[i].data['values'] += matrixes['values'][j][i]
+                for k in range(P):
+                    for var in variables:
+                        if var == 'values':
+                            continue
+                        C[i].data[var][k] += matrixes['values'][j][i][k]*matrixes[var][j][i][k]              
+
+            C[i].date, C[i].weekDay, C[i].month = PlotService.getDate(i + 1)
+
+        for i in range(N):
+            for j in range(P):
+                for var in variables:
+                    if var == 'values':
+                        continue
+                    if C[i].data['values'][j] != 0:
+                        C[i].data[var][j] /= C[i].data['values'][j]
+
+        # salvar os dados no json
+        print("Saving data to json")
+        with open('./backend/db/database.json', 'w') as file:
+            curve_data = {
+                "data_read": "True",
+                "curves": [curve.to_dict() for curve in C]
+            }
+            json.dump(curve_data, file, indent=4)
+
+    @staticmethod
+    def read_data(parameters: Parameters):
+        # ler os dados do json
+        with open('./backend/db/database.json') as json_file:
+            curve_data = json.load(json_file)
+        
+        if curve_data["data_read"] == "False":
+            PlotService.read_data_initial()
+        else:
+            print("Data already read")
+
+
+        # filtrar os dados de acordo com os parametros date_interval e days_of_week
+        C = []
+        with open('./backend/db/database.json') as json_file:
+            curve_data = json.load(json_file)
+            for curve in curve_data["curves"]:
+                date = datetime.strptime(curve["date"], "%Y-%m-%d")
+                if date >= datetime.strptime(parameters.date_interval[0], "%Y-%m-%d") and date <= datetime.strptime(parameters.date_interval[1], "%Y-%m-%d"):
+                    if date.weekday() in parameters.days_of_week:
+                        C.append(Curve.from_dict(curve))
+        return C
+
+    @staticmethod 
+    def computeED(parameters: Parameters):
+        r = [0.1, 0.2, 0.3, 0.4, 0.5]
+
+        query = QueryED(
+            r = r,
+            depthType = parameters.depth_type,
+            variables = parameters.variables,
+            hour_interval = parameters.hour_interval
+        )
+
+        C = PlotService.read_data(parameters)
+
+        ED(C, query)
+
+        # TODO: falta salvar no database   
+
+        return C
+    
+    @staticmethod
+    def transformToDataFrame(C):
+        data = []
+        cols = ['values', 'prices', 'distances', 'total_time', 'depth_g', 'extremal_depth', 'weekDay', 'id', 'date']
+        for curve in C:
+            data.append((curve.data['values'],
+                        curve.data['prices'],
+                        curve.data['distances'],
+                        curve.data['total_time'],
+                        curve.depth_g,
+                        curve.extremal_depth, 
+                        curve.weekDay,
+                        curve.id,
+                        curve.date))
+
+        df = pd.DataFrame(data, columns=cols)
+        return df
+    
+    @staticmethod
+    def calculateScatter(data, parameters: Parameters):
+        technique = parameters.dim_reduction_technique
+         
+        if technique == "UMAP":
+            model = umap.UMAP(n_components = 2, n_neighbors = 30, min_dist = 0.1, random_state=0)
+        else:
+            model = TSNE(n_components = 2, perplexity = 30, learning_rate = 100, random_state = 0)
+        
+        X = []
+        for i in range(len(data)):
+            X.append(data.at[i, 'depth_g'])
+
+        X = np.array(X)
+        y = model.fit_transform(X)
+
+        df = pd.DataFrame({'x': y[:, 0], 'y':  y[:, 1], 'id': data['id'], 'weekDay': data['weekDay']})
+
+        return df
+    
+    @staticmethod
+    def get_plot_data(parameters: Parameters):
+        if parameters.runED:
+            C = PlotService.computeED(parameters)
+        else:
+            C = PlotService.read_data(parameters)
+        
+        data = PlotService.transformToDataFrame(C)
+    
+        if parameters.plot == "scatter":
+            return PlotService.calculateScatter(parameters)
+        else:
+            return data
+
+if __name__ == "__main__":
+    PlotService.read_data()
+    # PlotService.computeED()
