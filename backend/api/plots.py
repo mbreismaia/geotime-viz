10ea0b808f5ab7b06bb10de3d1e6582503df3d82
Beatriz Maia <prbeatrizmaia@gmail.com>
2024-11-08 15:20:10 -0300

    add backend atualizations

--- backend/api/plots.py
+++ backend/api/plots.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 @router.get("/{plot_type}_plot", status_code=200, tags=["plots"])
 async def get_plot(plot_type: str, parameters: Parameters):
-
+    print('ENTROU NA FUNCAO DATA')
     data = PlotService.get_plot_data(parameters)
 
     if data is None:
